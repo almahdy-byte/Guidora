d38b38427dc2ef2ae47ef75c46df5a348d90609a
Bahi Habash <bahi.habash.edu@gmail.com>
2025-04-23 21:36:59 +0200

    RFACTORING

--- src/middleWare/allowTo.middleWare.js
+++ src/middleWare/allowTo.middleWare.js
@@ -1,9 +1,11 @@
-import { asyncErrorHandler } from "../utils/errorHandlers/asyncErrorHandler.js";
+import { asyncErrorHandler } from '../utils/errorHandlers/asyncErrorHandler.js';
 
-export const allowTo=(...roles)=>{
-    return (asyncErrorHandler((req , res, next)=>{
+export const allowTo = (...roles) => {
+    return asyncErrorHandler((req , res, next) => {
         const user = req.user;
+        
         if(!roles.includes(user.role)) return next(new Error('you are not allowed to access this end point'));
+
         next()
-    }))
-    }
+    });
+};
--- src/middleWare/auth.middleWare.js
+++ src/middleWare/auth.middleWare.js
@@ -3,8 +3,8 @@ import { asyncErrorHandler } from "../utils/errorHandlers/asyncErrorHandler.js"
 import { decodeToken } from "../utils/token/decodeToken.js";
 import { tokenTypes } from "../utils/globalEnums/enums.js";
 
-export const auth =()=>{
-    return asyncErrorHandler( async(req , res , next) => {
+export const auth = () => {
+    return asyncErrorHandler(async (req , res , next) => {
         const authorization = req.headers['authorization'];
         const decodedData = await decodeToken(authorization, tokenTypes.ACCESS, next);
 
@@ -12,11 +12,12 @@ export const auth =()=>{
             return next(new Error("Invalid refresh token", { cause: StatusCodes.UNAUTHORIZED }));
         }
 
-        const {user} = decodedData;
+        const { user } = decodedData;
 
         if (!user) throw new Error('user not found', { cause: StatusCodes.NOT_FOUND });
         
         req.user = user;
+        
         next()
     });
 }
